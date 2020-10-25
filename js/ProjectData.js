2a26ecfea2146b3ff4a018228d2cb2cc1da26f7d
Gaurav Kumar <gauravrmsc@gmail.com>
2020-10-25 19:10:24 +0530

    Resume Edited

--- js/ProjectData.js
+++ js/ProjectData.js
@@ -1,48 +1,46 @@
 const projectData = [
     {
         title : "Search Feature for Food Delivery Application",
-        desc : "This is a spring boot application that finds Near-By Restaurants search for a given location."+
-                " The application uses the Geo-Hashing technique to find Near-By Restaurants."+
-                " This application uses Redis Cache as an in-memory cache to cache the recent search requests. "+
-                "Response time for any cached result gets reduced by 80% resulting in a faster and seamless user experience.",
+        desc : "This is a spring boot application that finds Near-By Restaurants for a given location."+
+                " The application uses the Geo-Hashing technique to find Near-By Restaurants"+
+                " and Redis Cache as an in-memory cache to cache the search results. "+
+                "Response time for the cached results gets reduced to 80% of the normal response time resulting in a faster and seamless user experience.",
         skills : "Java, Spring Boot, Caching, Geo-Hashing, Serialization and Deserialization"+
                  "of Java Objects.",
         url :  "https://github.com/gauravrmsc/Food-Delivery-Application"
     },
     {
         title : "Stock Portfolio Manager",
-        desc : "Stock Portfolio Manager visual stock portfolio analyzer. It helps portfolio managers make trade recommendations for their clients. The application communicates with third-party APIs to collect Stock Quotes and presents users with the annualized and total returns on the given stock symbol.",
+        desc : "Stock Portfolio Manager is a visual stock portfolio analyser. It helps portfolio managers make trade recommendations for their clients. The application communicates with third-party APIs to collect Stock Quotes and presents user, the annualized and total returns on his purchased stocks.",
         skills : "Java, REST API, Jackson, Interfaces, Code Refactoring, Gradle, Exception Handling, Multithreading",
         url : "https://github.com/gauravrmsc/Stock-Portfolio-Manager"
     },
     {
         title : "Ecommerce API",
         desc : "Web Back-End for an E-Commerce Application. "+
-               "The application uses JWT based authentication and Spring Security to ensure authentication and authorization. "+
-               "The application is secure against CORS attacks, CSRF attacks and uses BCrypt password encoder to encode the passwords. ",
+               "The application uses JWT based authentication, BCrypt password encoder and Spring Security to secure the app against attacks like CORS attacks and CSRF attacks. ",
         skills : "Spring Security, Google Cloud, Jenkins, Authentication Using JWT, Using filters for authorization.",
         url : "https://github.com/gauravrmsc/EcommerceAPI"
     },
     {
         title :"O2Stay Selfie Challenge",
-        desc : "O2stay is a perfect website to look for lodging. My app helps the company to conduct"+
-                " promotional events on its Facebook page",
+        desc : "O2stay is a hospitality firm involved in providing lodging facilities. My application helps the company to host promotional events on its Facebook page.",
         skills : "GraphQL, Facebook API Handling, JSP, Servlets",
         url : "event.o2stay.com"
     },
     {
         title : "GQuiz",
-        desc : "GQuiz is a Q&A application. This application can be used to take online tests and quizes",
+        desc : "GQuiz is an Q&A application. This application can be used to take online tests and quizes",
         skills : "Object Relational Mapping, RESTful web services, JUnit, MySQL, Spring-Boot.",
         url : "https://github.com/gauravrmsc/Quiz-API"
     },
 
     {
         title : "GCode",
-        desc : "GCode is a web-based text editor. In this project, I implemented editor features like opening a file, navigation,"+
-               " search/replace, cut/copy/paste, undo/redo. While developing this project, I implemented the same functionality"+
+        desc : "GCode is a web-based text editor. In this project, I have implemented editor features like opening a file, navigation,"+
+               " search/replace, cut/copy/paste, undo/redo. While developing this project, I implemented the same functionality "+
                "with multiple data structures to understand the effect of design choices on performance.",
-        skills :"Applied DS, ArrayList, Naive Search, String manipulation, KMP search, Linked List, Time Complexity",
+        skills :"Applied DS, Array-List, Naive Search, String manipulation, KMP search, Linked-List, Time Complexity",
         url :"https://github.com/gauravrmsc/Online-Text-Editor"
     }    
 ];
